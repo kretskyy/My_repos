44aade8ccc7737960990027e6c5f875d72fe12ce
kretskyy <deonisius03@gmail.com>
2021-11-23 16:28:14 +0200

    update

--- Project2/PizzaRepository.h
+++ Project2/PizzaRepository.h
@@ -7,7 +7,7 @@ using namespace std;
 class PizzaRepository: public Repository<Pizza>
 {
 protected:
-	string GetFileName() { return "Pizza.txt"; }
+	string GetFileName() { return "C:\\Users\\Akret\\source\\repos\\Project2\\Project2\\Pizza.txt"; }
 	string GetTitle() { return "Pizza: "; }
 public:
 	PizzaRepository(int max) : Repository(max)
